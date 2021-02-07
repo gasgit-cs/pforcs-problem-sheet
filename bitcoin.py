367cfb08ebdbcc050bc0c4fb6fd2528e586f9e37
gasgit-cs <G00305555@gmit.ie>
2021-02-07 20:02:21 +0000

    update to v2 bmi

--- bitcoin.py
+++ bitcoin.py
@@ -3,49 +3,57 @@
 
 # v1 intial script breakdown of components
 
-# import module requests for making http get request to api
-import requests
-# import module pprint( prints json in easy formay for humans to read )
-import pprint
+# # import module requests for making http get request to api
+# import requests
+# # import module pprint( prints json in easy formay for humans to read )
+# import pprint
 
-# create url to the api
-url_to_get = "https://api.coindesk.com/v1/bpi/currentprice.json"
+# # create url to the api
+# url_to_get = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
-# create a var ( my_data ) to store the returned data from the get request
-# pass the url_to_get to requests get and assign results to my_data
-my_data = requests.get(url_to_get)
-#print(my_data) # test output
-# convert the response to json
-bc_dict = my_data.json() 
+# # create a var ( my_data ) to store the returned data from the get request
+# # pass the url_to_get to requests get and assign results to my_data
+# my_data = requests.get(url_to_get)
+# #print(my_data) # test output
+# # convert the response to json
+# bc_dict = my_data.json() 
 
-#print(type(bc_dict)) #test type
-# print the object to readable format using the pretty print module
-pprint.pprint(bc_dict)
-# add title 
-print("Rates Now :) || :(")
-# unpack tuples by iterating over the dict items
-for bc_rate in bc_dict['bpi'].items():
+# #print(type(bc_dict)) #test type
+# # print the object to readable format using the pretty print module
+# pprint.pprint(bc_dict)
+# # add title 
+# print("Rates Now :) || :(")
+# # unpack tuples by iterating over the dict items
+# for bc_rate in bc_dict['bpi'].items():
 
-    #print(type(bc_rate)) #test type
-    # use a, b variables for unpacking
-    a, b = bc_rate
-#region
-    # unpack currency
-    # print(type(a)) #test type
-    # print(a) #test 
-    # unpack rate
-    # print(type(b)) #test
-    # print(b['rate']) #test
-#endregion
+#     #print(type(bc_rate)) #test type
+#     # use a, b variables for unpacking
+#     a, b = bc_rate
+# #region
+#     # unpack currency
+#     # print(type(a)) #test type
+#     # print(a) #test 
+#     # unpack rate
+#     # print(type(b)) #test
+#     # print(b['rate']) #test
+# #endregion
 
-    print("{} current rate of bitcoin is {}".format(a, b['rate']))
+#     print("{} current rate of bitcoin is {}".format(a, b['rate']))
 
+
+##############################################################################
 
 # v2 complete the program using functions and a main function to call from
 # add try, except to catch any connection issues
 # 
-#import requests
-#import pprint
+import requests
+import pprint
+
+
+# things to do
+# update try, except
+# update comments to v2 
+# move v1 to commented out section at the bottom or delete
 
 
 def make_request(url_to_get):
--- bmi.py
+++ bmi.py
@@ -3,38 +3,96 @@
 # outputs height, metres, weight in kgs, bmi 
 # author glen gardiner
 
+###################################################################################
+# v1
 # print new blank line
-print("\n")
+# print("\n")
 
-# display message to user
-print("We need some details to calculate:)")
+# # display message to user
+# print("We need some details to calculate:)")
 
-# print new blank line
-print("\n")
+# # print new blank line
+# print("\n")
 
-# get user height in cms
-user_height_cms = float(input("Emter your height cms: "))
+# # get user height in cms
+# user_height_cms = float(input("Emter your height cms: "))
 
-# convert cms to metres
-user_height_metres = user_height_cms / 100
+# # convert cms to metres
+# user_height_metres = user_height_cms / 100
 
-# print height in metres
-print("Height: " + str(user_height_metres))
+# # print height in metres
+# print("Height: " + str(user_height_metres))
 
-# get user weight in kgs
-user_weight = float(input("Enter your weight kg: "))
+# # get user weight in kgs
+# user_weight = float(input("Enter your weight kg: "))
 
-# print user weight
-print("Weight: " + str(user_weight))
+# # print user weight
+# print("Weight: " + str(user_weight))
 
-# calculate bmi as userweight divided by height in metres squared
-bmi = (user_weight / (user_height_metres ** 2))
-abs_bmi = abs((user_weight / (user_height_metres ** 2)))
+# # calculate bmi as userweight divided by height in metres squared
+# bmi = (user_weight / (user_height_metres ** 2))
+# abs_bmi = abs((user_weight / (user_height_metres ** 2)))
 
-# print bnew lank line
-print("\n")
+# # print bnew lank line
+# print("\n")
 
-# print bmi message, print with string function after round to 2 decimal places
-#print("Your BMI is : " + str(round(bmi, 2)))
-print("Your BMI is : " + str(abs_bmi))
+# # print bmi message, print with string function after round to 2 decimal places
+# #print("Your BMI is : " + str(round(bmi, 2)))
+# print("Your BMI is : " + str(abs_bmi))
+
+##################################################################################
+
+# v2 complete program with main and functions
+
+def get_height():
+    # print new blank line
+    print("\n")
+
+    # display message to user
+    print("We need some details to calculate:)")
+
+    # print new blank line
+    print("\n")
+
+    # get user height in cms
+    user_height_cms = float(input("Emter your height cms: "))
+
+    # convert cms to metres
+    user_height_metres = user_height_cms / 100
+
+    # print height in metres
+    print("Height: " + str(user_height_metres))
+
+    # return height in metres
+    return user_height_metres
+
+def get_weight():
+
+    user_weight = float(input("Enter your weight kg: "))
+    print("Weight: " + str(user_weight))
+    return user_weight
+
+# function to calculate the bmi, take 2 args user weigh & user height in metres
+def calculate(user_weight, user_height_metres):
+
+    bmi = (user_weight / (user_height_metres ** 2))
+    # return bm1
+    return bmi
+
+# function to display bmi, accepts 1 args bmi
+def display(bmi):
+    # print message and bmi
+    print("Your BMI is : " + str(bmi))
+
+# main method for program, call other functions as required
+def main():
+    
+    x = get_height()
+    y = get_weight()
+    b = calculate(y, x)
+    display(b)
+
+# conditional block to check if the script contains a main function, running while True
+if __name__ == "__main__":
+    main()
 
