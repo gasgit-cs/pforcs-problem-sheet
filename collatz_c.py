26390fa79453922ebe076a547aba364cbb59bfaa
gasgit-cs <G00305555@gmit.ie>
2021-02-13 01:03:47 +0000

    update functions, name, output

--- collatz_c.py
+++ collatz_c.py
@@ -1,149 +1,58 @@
 # progarm to demonstarte collattz conjuncture
-# take an int number from the user, and follow the following steps
-# check value
-# if its is even divide it by 2
-# if it is odd,multiply it by 3 and add 1
+# take a int from the user, and follow steps
+# check is valid int(sign)
+# if its is even divide by 2
+# if it is odd,multiply by 3 and add 1
+# return, store and display values
 
 # author glen gardiner
 
+# list data structure to store values
+col_list = []
 
-#################################################################################
-
-# v2
-
-# take an int number from the user, and follow the following steps
-# create function to check
-# if its is even divide it by 2 and return as current value
-# if it is odd,multiply it by 3 and add 1 return as current value
-# using a while loop to keep checking as long as the current value is not 1
-
-
-# get value
-# check sign
-# check parity
-# calculate value
-# return current value even or odd
-# if not 1 check again
-
-
-def check_par(next_val):
-    if (next_val % 2) == 0:
-        curr_val = next_val // 2
-        print(curr_val,  end=" ")
-        return curr_val
-    else:
-        curr_val = (next_val * 3) + 1
-        print(curr_val, end=" ")
-        return curr_val
-
+# get and return int fron user
 def get_input():
-    val_1 = int(input("Enter int: "))
-    return val_1
+    i = int(input("Enter int: "))
+    return i
 
-# check parity, if not negative and greater then 0 return, else return get input
-def check_sign(val_1):
-    if val_1 != type(int()) and val_1 > 0:
-        print("Returned", str(val_1))
-        return val_1
+# check sign, if not greater than 0, get input and check sign again
+def check_sign(s):
+    if s > 0:
+        return s
     else:
         print("Not Positive Input, Try Again:)")
-        print("Rejected", str(val_1))
-        x = get_input()
-        return check_sign(x)
-        
-# while 
-def meet_condition(next_val):
-    while next_val != 1:
-        # if next_val < 0:
-        #     print(next_val)
-            # print("Unfortunatly 2 negitives do not make a positive in this instance :(")
-            # break
-        next_val = check_par(next_val)
+        i = get_input()
+        return check_sign(i)
+
+#  check parity, do calculation, append to list and return value
+def check_par(cp):
+    if (cp % 2) == 0:
+        curr_val = cp // 2
+        col_list.append(curr_val)
+        return curr_val
     else:
-        # print exit message once 1 is returned
-        print("Exited by 1 :()")
+        curr_val = (cp * 3) + 1
+        col_list.append(curr_val)
+        return curr_val
 
+# while condition is true, check and return or break
+def meet_condition(mc):
+    while True:
+        if mc != 1:
+            mc = check_par(mc)
+        else:
+            break
+ # iiterate list and display       
+def display_col_list():
+    for x in col_list:
+        print(x, end=" ")
+
+# main function to organise/control program flow
 def main():
-
-    input1 = get_input()
-    next_val = check_sign(input1)
-    meet_condition(next_val)
-
+    cs = get_input()
+    mc = check_sign(cs)
+    meet_condition(mc)
+    display_col_list()
 
 if __name__ == "__main__":
     main()
-
-# while next_val != 1:
-#     next_val = check_value(next_val)
-# else:
-#     # print exit message once 1 is returned
-#     print("Exited by 1 :()")
-
-
-# conditional while loop, ture until returned value from check_value is not == 1
-
-
-
-#################################################################################
-
-# v3
-
-# recursive
-# 
-# get value
-# check sign
-# check parity
-# calculate value
-# return current value even or odd
-# if not 1 check again
-
-# def check_recursive(val_1):
-#     # check if value is even with modulo
-#     if(val_1 % 2) == 0:
-#         # assign val divided by  2
-#         curr_v = val_1 // 2
-#         # check if current value is not == 1
-#         if curr_v != 1:
-#             print(curr_v)
-#             # if not pass to check function
-#             check_recursive(curr_v)
-#         else:
-#             # current value is 1, exit
-#             print("{} Exit ".format(curr_v))
-#     else:
-#         # current value is odd, do calculation
-#         curr_v = (val_1 * 3) + 1
-#         # check if current value is == 1
-#         if curr_v != 1:
-#             print(curr_v)
-#             # if not pass to check_function
-#             check_recursive(curr_v)
-#         else:
-#             # current value is 1, exit
-#             print("{} Exit ".format(curr_v))
-# # get input 
-# def get_pos_input():
-#     
-#     val_1 = int(input("Enter int please V3: "))
-#     return val_1
-# # check parity, if not negative and greater then 0 return, else return get input
-# def check_par(val_1):
-#     if val_1 != type(int()) and val_1 > 0:
-#         #print("Returned", str(val_1))
-#         return val_1
-#     else:
-#         print("Not Positive Input, Try Again:)")
-#         return get_pos_input()
-   
-# def main():
-#     x = get_pos_input()
-#     y = check_par(x)
-#     # pass y as val_1 to check_recurrsive
-#     check_recursive(y)
-
-# if __name__ == "__main__":
-#     main()
-
-
-
-
