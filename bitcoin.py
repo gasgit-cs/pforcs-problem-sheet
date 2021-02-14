0b21d227bf1882b2a42fc05ee884e4f16c895947
gasgit-cs <G00305555@gmit.ie>
2021-02-14 01:52:16 +0000

    update, args, typos

--- bitcoin.py
+++ bitcoin.py
@@ -4,9 +4,9 @@
 # v1 intial script breakdown of components
 
 # # import module requests for making http get request to api
-# import requests
+# #import requests
 # # import module pprint( prints json in easy formay for humans to read )
-# import pprint
+# # import pprint
 
 # # create url to the api
 # url_to_get = "https://api.coindesk.com/v1/bpi/currentprice.json"
@@ -16,12 +16,12 @@
 # my_data = requests.get(url_to_get)
 # #print(my_data) # test output
 # # convert the response to json
-# bc_dict = my_data.json() 
+# bc_dict = my_data.json()
 
 # #print(type(bc_dict)) #test type
 # # print the object to readable format using the pretty print module
 # pprint.pprint(bc_dict)
-# # add title 
+# # add title
 # print("Rates Now :) || :(")
 # # unpack tuples by iterating over the dict items
 # for bc_rate in bc_dict['bpi'].items():
@@ -32,7 +32,7 @@
 # #region
 #     # unpack currency
 #     # print(type(a)) #test type
-#     # print(a) #test 
+#     # print(a) #test
 #     # unpack rate
 #     # print(type(b)) #test
 #     # print(b['rate']) #test
@@ -45,14 +45,14 @@
 
 # v2 complete the program using functions and a main function to call from
 # add try, except to catch any connection issues
-# 
+#
 import requests
 import pprint
 
 
 # things to do
 # update try, except
-# update comments to v2 
+# update comments to v2
 # move v1 to commented out section at the bottom or delete
 
 
@@ -63,27 +63,36 @@ def make_request(url_to_get):
         my_data.raise_for_status()
         print(my_data)
     except requests.exceptions.HTTPError as e:
-        print (e.response.text)
+        print(e.response.text)
     my_data = requests.get(url_to_get)
     return my_data
 
+
 def convert_my_data(my_data):
-    # function to parse and print 
-    bc_dict = my_data.json() 
+    # function to parse and print
+    # convert the response to json
+    bc_dict = my_data.json()
+    # print the object to readable format using the pretty print module
     pprint.pprint(bc_dict)
+    # add title
     print("Rates Now :) || :(")
+    # unpack tuples by iterating over the dict items
     for bc_rate in bc_dict['bpi'].items():
         a, b = bc_rate
         print("{} current rate of bitcoin is {}".format(a, b['rate']))
 
 # main function as entry point to the program, calling other frinctions
+
+
 def main():
     # assign full url to url_to_get var
+    # pass the url_to_get to requests get and assign results to url_to_get
     url_to_get = "https://api.coindesk.com/v1/bpi/currentprice.json"
     # pass url_to_get to make request function, assign the returned value to data
     data = make_request(url_to_get)
     # pass datto convert function
     convert_my_data(data)
+
 
 # conditional block to check if the script contains a main function, running while True
 if __name__ == "__main__":
--- collatz_c.py
+++ collatz_c.py
@@ -1,4 +1,4 @@
-# progarm to demonstarte collattz conjuncture
+# program to demonstarte collattz conjuncture
 # take a int from the user, and follow steps
 # check is valid int(sign)
 # if its is even divide by 2
@@ -8,14 +8,17 @@
 # author glen gardiner
 
 # list data structure to store values
-col_list = []
+# cl - colatz list to store data
+cl = []
 
 # get and return int fron user
 def get_input():
+    # i - input value
     i = int(input("Enter int: "))
     return i
 
 # check sign, if not greater than 0, get input and check sign again
+# s - passed value to check sign
 def check_sign(s):
     if s > 0:
         return s
@@ -24,34 +27,42 @@ def check_sign(s):
         i = get_input()
         return check_sign(i)
 
-#  check parity, do calculation, append to list and return value
-def check_par(cp):
-    if (cp % 2) == 0:
-        curr_val = cp // 2
-        col_list.append(curr_val)
-        return curr_val
+# check parity, do calculation, append to list and return value
+# if (p / 2 == 0 )  p is even
+# p - passed value to check parity
+# cv - current value
+# cl - list to store data
+def check_par(p):
+    if (p % 2) == 0:
+        cv = p // 2
+        cl.append(cv)
+        return cv
     else:
-        curr_val = (cp * 3) + 1
-        col_list.append(curr_val)
-        return curr_val
+        cv = (p * 3) + 1
+        cl.append(cv)
+        return cv
 
-# while condition is true, check and return or break
-def meet_condition(mc):
+# while loop to check condition( c is not equal to 1 ) is true proceed
+#  until false break
+# c -  passed value to check condition
+def meet_condition(c):
     while True:
-        if mc != 1:
-            mc = check_par(mc)
+        if c != 1:
+            c = check_par(c)
         else:
+            print("{} exit!".format(c))
             break
- # iiterate list and display       
+ # iiterate list and display in sequence   
 def display_col_list():
-    for x in col_list:
+    print("Collatz Result")
+    for x in cl:
         print(x, end=" ")
 
 # main function to organise/control program flow
 def main():
-    cs = get_input()
-    mc = check_sign(cs)
-    meet_condition(mc)
+    x = get_input()
+    y = check_sign(x)
+    meet_condition(y)
     display_col_list()
 
 if __name__ == "__main__":
